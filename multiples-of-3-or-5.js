97c54a861834ee32846ea13fd6c36f96b2d6ae6e
mannoeu <emmanuel_costasilva@hotmail.com>
2022-04-11 23:00:48 -0300

    problem 1

---
+++ multiples-of-3-or-5.js
@@ -0,0 +1,14 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// Solution:
+// As the question already gives the multiples of 3 or 5 less than 10, I will start the vector with the multiples and iterate from 10
+let multiples = [3, 5, 6, 9];
+
+for (let i = 10; i < 1000; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    multiples.push(i);
+  }
+}
+
+multiples.reduce((prev, cur) => prev + cur, 0);
